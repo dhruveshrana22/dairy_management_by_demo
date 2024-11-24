a1e82c33fcd48a379e705d29c04f7eca80718d30
dhruveshrana22 <dcrana2222@gmail.com>
2024-11-24 22:44:05 +0530

    add the branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Header from '@/components/Header/Header';
 import LoginForm from '@/components/singinCompoents/LoginForm';
 
 export default function Home() {
   return (
     <>
+      <div>dhruvesh Rana How R you I am the devloper give the</div>
+      <Header />
       <LoginForm />
     </>
   );
